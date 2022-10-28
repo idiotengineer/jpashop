fd4c9a6457fbc44448be3d7492e9deee6d96a968
idiotengineer <gurtjd97@gmail.com>
2022-10-28 20:12:42 +0900

    6. 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,40 +6,18 @@ import javax.persistence.Embeddable;
 
 @Embeddable // JPA의 내장타입임을 선언
 @Getter
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-public class Address {
-=======
 public class Address { // 값 타입은 변경불가능하게 해야함 -> setter가 없음
->>>>>>> topic
-=======
-public class Address { // 값 타입은 변경불가능하게 해야함 -> setter가 없음
->>>>>>> topic3
 
     private String city;
     private String street;
     private String zipcode;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
     protected Address() {
     }
 
-=======
-    protected Address() {
-    }
-
->>>>>>> topic3
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
         this.zipcode = zipcode;
     }
-<<<<<<< HEAD
->>>>>>> topic
-=======
->>>>>>> topic3
 }
--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -25,33 +25,16 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name="item_id"))
     private List<Item> items = new ArrayList<>();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @ManyToOne
-=======
     @ManyToOne(fetch = FetchType.LAZY)
->>>>>>> topic
-=======
-    @ManyToOne(fetch = FetchType.LAZY)
->>>>>>> topic3
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> topic3
     // == 연관관계 메서드 == //
     public void addChildCategory(Category child) {
         this.child.add(child);
         child.setParent(this);
     }
-<<<<<<< HEAD
->>>>>>> topic
-=======
->>>>>>> topic3
 }
